17ad370b3cd1e230a2040d519c7e99d429e4bb41
GodHongIl <adunstudio@daum.net>
2025-01-25 23:39:35 +0900

    mps teset

--- application/strategy/local_whisper_strategy.py
+++ application/strategy/local_whisper_strategy.py
@@ -7,7 +7,9 @@ from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 
 class LocalWhisperStrategy(STTStrategy):
     def __init__(self, model_name: str):
-        device = "cuda" if torch.cuda.is_available() else "cpu"
+        device = "cuda" if torch.cuda.is_available() else \
+                 "mps" if torch.backends.mps.is_available() else \
+                 "cpu"
         torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
         model = AutoModelForSpeechSeq2Seq.from_pretrained(model_name, torch_dtype=torch_dtype)
